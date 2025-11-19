43c8e3f008cc016733bd8557d686633813554dbc
Adel <ashamsutov@corp.local>
2025-11-19 13:55:21 +0300

    Добавил инициацию БД и затем её очистку

--- cmd/auth_test.go
+++ cmd/auth_test.go
@@ -3,19 +3,58 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"io"
 	"links-shortener/internal/auth"
+	"links-shortener/internal/user"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
+const (
+	TestEmail = "aaa23@ya.ru"
+)
+
+func initDb() *gorm.DB {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func initData(db *gorm.DB) {
+	db.Create(&user.User{
+		Email:    TestEmail,
+		Name:     "Вася Тестов",
+		Password: "$2a$10$/bGD7PqJXPKUnFa9oSz1Yuh8ksf.1G8zJ9nSxFsFyna631KEqYX16",
+	})
+}
+
+func removeData(db *gorm.DB) {
+	db.Unscoped().
+		Where("email = ?", TestEmail).
+		Delete(&user.User{})
+}
+
 func TestLoginSuccess(t *testing.T) {
+	// Prepare
+	db := initDb()
+	initData(db)
+
 	ts := httptest.NewServer(App())
 	defer ts.Close()
 
 	data, _ := json.Marshal(&auth.LoginRequest{
-		Email:    "aaa23@ya.ru",
+		Email:    TestEmail,
 		Password: "passs",
 	})
 
@@ -38,9 +77,14 @@ func TestLoginSuccess(t *testing.T) {
 	if resData.Token == "" {
 		t.Fatal("expected token, got empty")
 	}
+	removeData(db)
 }
 
 func TestLoginFail(t *testing.T) {
+	// Prepare
+	db := initDb()
+	initData(db)
+
 	ts := httptest.NewServer(App())
 	defer ts.Close()
 
@@ -56,4 +100,5 @@ func TestLoginFail(t *testing.T) {
 	if res.StatusCode != http.StatusUnauthorized {
 		t.Fatalf("expected status code %d, got %d", http.StatusUnauthorized, res.StatusCode)
 	}
+	removeData(db)
 }
